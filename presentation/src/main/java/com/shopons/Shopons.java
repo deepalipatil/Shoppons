36ddbf62b291d8e511287671c34463374b5a9ad1
deeps <dmpatil1995@gmail.com>
2016-07-28 18:59:24 +0530

    apk included

--- presentation/src/main/java/com/shopons/Shopons.java
+++ presentation/src/main/java/com/shopons/Shopons.java
@@ -16,7 +16,8 @@ public class Shopons extends MultiDexApplication {
      * or receiver objects (excluding content providers) have been created.
      * Implementations should be as quick as possible (for example using
      * lazy initialization of state) since the time spent in this function
-     * directly impacts the performance of starting the first activity,
+     *
+     * impacts the performance of starting the first activity,
      * service, or receiver in a process.
      * If you override this method, be sure to call super.onCreate().
      */
